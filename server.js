ab3133b3c57bf4f92777e4fe56d0238ca1a5d8f8
Evon Perez <perez.evon@gmail.com>
2023-02-25 16:03:18 -0500

    changed process.env.DATABASE_URLAPI_URL to process.env.DATABASE_URL

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./lib/app');
 const pool = require('./lib/utils/pool');
 
-const API_URL = process.env.API_URL || 'http://localhost';
+const API_URL = process.env.DATABASE_URL || 'http://localhost';
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
